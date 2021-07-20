a990562f0047ce197e77e37c9581f9f382e91ae7
Dennis Iversen <dennis.iversen@gmail.com>
2021-07-20 14:30:54 +0200

    some typos

--- utils_test.php
+++ utils_test.php
@@ -33,21 +33,21 @@ echo $utils->colorOutput("Executing a command that does not exists (dudida) - bu
 $res = $utils->execCommand('dudida', 0, 0);
 echo $utils->colorOutput("$res is the result of the above operation\n", 'light_blue');
 
-echo $utils->colorOutput("Exectuing a command that that properbly exists (dir)\n");
-$res = $utils->execCommand('dir');
+echo $utils->colorOutput("Exectuing a command that that properbly exists (ls)\n");
+$res = $utils->execCommand('ls -l');
 echo $utils->colorOutput("$res is the result of the above operation\n", 'light_blue');
 
-echo $utils->colorOutput("Exectuing a command that that properbly exists (dir) - but without status and command messages\n");
-$res = $utils->execCommand('dir', 0, 0);
+echo $utils->colorOutput("Exectuing a command that that properbly exists (ls) - but without status and command messages\n");
+$res = $utils->execCommand('ls -l', 0, 0);
 echo $utils->colorOutput("$res is the result of the above operation\n", 'light_blue');
 
 echo $utils->colorOutput ("All is almost identically with \$utils->systemCommand\n", 'bg_blue');
 echo $utils->colorOutput ("But \$utils->systemCommand will always echo output from the executed command", 'bg_blue');
 
 echo $utils->colorOutput("\nEcho a red status message\n");
-echo $utils->colorOutput("STATUS ", 'light_green', 'A status about how we are doing it');
+echo $utils->colorOutput("STATUS ", 'red', 'A status about how we are doing!!!');
 
-echo $utils->colorOutput("Prompt the user for input\n");
+echo $utils->colorOutput("Prompt the user for a sinlge line of input\n");
 $line = $utils->readSingleline('Command will read a line: ');
 echo $utils->colorOutput("You wrote $line\n");
 
