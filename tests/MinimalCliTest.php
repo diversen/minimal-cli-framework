971d8a646bf56c376ee91f45c79893cc97667367
Dennis Iversen <dennis.iversen@gmail.com>
2023-03-22 17:47:39 +0100

    better testing

--- tests/MinimalCliTest.php
+++ tests/MinimalCliTest.php
@@ -17,15 +17,60 @@ final class MinimalCliTest extends TestCase
 
     public function testConstruct()
     {
-        $minimal_cli = new MinimalCli();
+
         
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->setTestMode();
+
         $this->assertInstanceOf(MinimalCli::class, $minimal_cli);
 
         $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $this->assertInstanceOf(EchoTest::class, $minimal_cli->commands['echo']);
 
-        // Only these public methods but should be made more testable
-        // /$minimal_cli->setHeader("Program Test ECHO command");
-        // $minimal_cli->runMain();
+        // No command specified
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(1, $res);
+
+        // Correct command given
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php', 'echo', '--up', 'README.md']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(0, $res);
+
+        // Command that do not exists
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php', 'do_not_exists', '--up', 'README.md']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(1, $res);
+
+
+        // Return value from command. Should be 12 as file does not exists
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php', 'echo', '--up', 'WRONG_FILE.md']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(12, $res);
+
+        // Valid command alias 'e'
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php', 'e', '--up', 'README.md']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(0, $res);
+
+        // Valid option alias '-u'
+        $minimal_cli = new MinimalCli();
+        $minimal_cli->addCommandClass('echo', EchoTest::class);
+        $minimal_cli->setTestMode(argv: ['test.php', 'e', '-u', 'README.md']);
+        $res = $minimal_cli->runMain();
+        $this->assertEquals(0, $res);
+
+
         
     }
     
